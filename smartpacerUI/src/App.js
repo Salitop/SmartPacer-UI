5aca81f027c4e642986a96983b8f8c10e5583946
IsraelAugusto0110 <56437566+IsraelAugusto0110@users.noreply.github.com>
2023-03-28 09:33:22 -0300

    Movi Login e Home para o diretorio Pages

--- smartpacerUI/src/App.js
+++ smartpacerUI/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
