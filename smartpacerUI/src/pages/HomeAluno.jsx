c1ad4f23d4d2bf46518046a168b99db5a5c85f1a
IsraelAugusto0110 <56437566+IsraelAugusto0110@users.noreply.github.com>
2023-03-30 11:40:56 -0300

    Adicionada a função de logout no frontend

--- smartpacerUI/src/pages/HomeAluno.jsx
+++ smartpacerUI/src/pages/HomeAluno.jsx
@@ -1,12 +1,36 @@
-import { Grid, Typography } from "@mui/material";
-import React from "react";
+import { Button, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Service from "../Service";
 
 function HomeAluno() {
+  const navigate = useNavigate();
+  const [id, setId] = useState();
+  const [nome, setNome] = useState();
+
+  const logout = async () => {
+    await Service.post("//localhost:5000/logout");
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await Service.get("//localhost:5000/@me");
+        setId(resp.data.id);
+        setNome(resp.data.nome);
+      } catch (error) {
+        console.log("Not authenticated");
+      }
+    })();
+  });
+
   return (
     <>
       <Grid container flexDirection="column" alignItems="center">
         <Grid item sx={{ marginTop: 10 }}>
-          <Typography variant="h3"> Smart Pacer - Bem vindo Aluno</Typography>
+          <Typography variant="h3">Smart Pacer - Bem vindo Aluno</Typography>
+          <Button onClick={() => logout()}>Sair</Button>
         </Grid>
       </Grid>
     </>
--- smartpacerUI/src/pages/HomeProfessor.jsx
+++ smartpacerUI/src/pages/HomeProfessor.jsx
@@ -1,7 +1,29 @@
-import { Grid, Typography } from "@mui/material";
-import React from "react";
+import { Button, Grid, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Service from "../Service";
 
 function HomeProfessor() {
+  const navigate = useNavigate();
+  const [id, setId] = useState();
+  const [nome, setNome] = useState();
+
+  const logout = async () => {
+    await Service.post("//localhost:5000/logout");
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await Service.get("//localhost:5000/@me");
+        setId(resp.data.id);
+        setNome(resp.data.nome);
+      } catch (error) {
+        console.log("Not authenticated");
+      }
+    })();
+  });
   return (
     <>
       <Grid container flexDirection="column" alignItems="center">
@@ -9,6 +31,7 @@ function HomeProfessor() {
           <Typography variant="h3">
             Smart Pacer - Bem vindo Professor
           </Typography>
+          <Button onClick={() => logout()}>Sair</Button>
         </Grid>
       </Grid>
     </>
