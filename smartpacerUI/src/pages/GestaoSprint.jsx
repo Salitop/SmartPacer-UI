3c03c730a0fb37d7870695c81d33c781821c87d7
JulioL2001 <julio.lucena.2001@gmail.com>
2023-04-13 12:18:34 -0300

    Desenvolvimento da tela de gestão de Sprints

--- smartpacerUI/src/pages/GestaoSprint.jsx
+++ smartpacerUI/src/pages/GestaoSprint.jsx
@@ -1,7 +1,31 @@
+import { Container, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 
 function GestaoSprint() {
-  return <div>GestaoSprint</div>;
+  return (
+    <div>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Paper elelvation={2} sx={{ padding: 3 }}>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              textAlign="center"
+              sx={{ paddingBottom: 5 }}
+            >
+              Gestão de Sprints
+            </Typography>
+          </Paper>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default GestaoSprint;
