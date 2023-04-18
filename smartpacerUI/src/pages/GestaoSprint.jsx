cd27c97251363e68a91e862167434237925b16d0
JulioL2001 <julio.lucena.2001@gmail.com>
2023-04-18 10:13:38 -0300

    Feitas alterações na tela de gestão de sprint- adicionado o formulario para cadastro de nota

--- smartpacerUI/src/pages/GestaoSprint.jsx
+++ smartpacerUI/src/pages/GestaoSprint.jsx
@@ -1,7 +1,43 @@
-import { Container, Grid, Paper, Typography } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 function GestaoSprint() {
+  const navigate = useNavigate();
+  const [equipe, setEquipe] = useState();
+  const [sprint, setSprint] = useState();
+  const [notaPacer, setNotaPacer] = useState(0);
+
+  const handleChangeSprint = (event) => {
+    setSprint(event.target.value);
+  };
+
+  const handleChangeEquipe = (event) => {
+    setEquipe(event.target.value);
+  };
+
+  const handleChangeNotaPacer = (event) => {
+    setNotaPacer(event.target.value);
+  };
+
+  const handleCadastrarNotaPacer = () => {
+    console.log(equipe, sprint, notaPacer);
+  };
+
+  const handleVoltarParaHome = () => {
+    navigate("/home-prof");
+  };
+
   return (
     <div>
       <Container maxWidth="lg">
@@ -21,6 +57,59 @@ function GestaoSprint() {
             >
               Gestão de Sprints
             </Typography>
+            <form>
+              <Grid container direction="column" spacing={4}>
+                <Grid item sx={{ alignSelf: "center", width: 500 }}>
+                  <Select
+                    value={sprint}
+                    onChange={handleChangeSprint}
+                    fullWidth
+                    name="Sprint"
+                  >
+                    <MenuItem value={1}>Sprint 1</MenuItem>
+                    <MenuItem value={2}>Sprint 2</MenuItem>
+                  </Select>
+                </Grid>
+                <Grid item sx={{ alignSelf: "center", width: 500 }}>
+                  <Select
+                    value={equipe}
+                    onChange={handleChangeEquipe}
+                    fullWidth
+                    name="Equipe"
+                  >
+                    <MenuItem value={1}>Equipe 1</MenuItem>
+                    <MenuItem value={2}>Equipe 2</MenuItem>
+                  </Select>
+                </Grid>
+                <Grid
+                  item
+                  sx={{
+                    alignSelf: "center",
+                    width: 500,
+                  }}
+                >
+                  <TextField
+                    type="number"
+                    label="Nota Pacer"
+                    onChange={handleChangeNotaPacer}
+                    value={notaPacer}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    onClick={handleCadastrarNotaPacer}
+                  >
+                    Cadastrar Nota Pacer
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={handleVoltarParaHome}>
+                    Voltar
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
           </Paper>
         </Grid>
       </Container>
