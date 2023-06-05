e3381bd3969facb1f519732c482dd45f8183305f
Luiz Miguel <luizmiguelandre332@gmail.com>
2023-06-05 16:59:14 -0300

    Correção de apontamentos

--- smartpacerUI/src/pages/GestaoSprint.jsx
+++ smartpacerUI/src/pages/GestaoSprint.jsx
@@ -1,9 +1,7 @@
 import {
   Button,
   Container,
-  FormControl,
   Grid,
-  InputLabel,
   MenuItem,
   Paper,
   TextField,
@@ -25,6 +23,7 @@ function GestaoSprint() {
   const [sprintsList, setSprintsList] = useState([]);
   const [idEquipe, setIdEquipe] = useState();
   const [sprintId, setSprintId] = useState();
+
   // obtem o semestre e ano atual
   React.useEffect(() => {
 
@@ -69,9 +68,22 @@ function GestaoSprint() {
     }
   }, [sprints]);
 
-  const handleCadastrarNotaPacer = () => {
-    console.log(equipe, sprint, notaPacer);
+  const fetchCadastrarNotapacer = async () => {
+    await Axios.post(`https://edryanmaciel.pythonanywhere.com/cadastrarNotaSprint`, {
+      idEquipe,
+      sprintId,
+      notaPacer,
+    }).then((response) => {
+      console.log(response);
+    });
+    console.log(
+      "Sprint: " + sprintId + " Equipe: " + idEquipe + " Nota: " + notaPacer
+    );
   };
+
+  function handleFetchCadastrarNotaPacer() {
+    fetchCadastrarNotapacer();
+  }
 
   const handleVoltarParaHome = () => {
     navigate("/home-prof");
@@ -84,41 +96,8 @@ function GestaoSprint() {
         <Typography variant="h4" fontWeight="bold" textAlign="center" sx={{ paddingBottom: 5 }}>Gestão Sprint</Typography>
           <Paper elelvation={2} sx={{ padding: 5, width: 500 }}>
               <Grid item>
-                <Grid
-                  item
-                  sx={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(3, 1fr)",
-                  }}
-                >
-                  <TextField
-                    type="text"
-                    label="Semestre"
-                    variant="outlined"
-                    sx={{ width: 150, justifySelf: "right" }}
-                    required
-                    onChange={(e) => setSemestre(e.target.value)}
-                  />
-                  <TextField
-                    type={"text"}
-                    label="Ano"
-                    variant="outlined"
-                    sx={{ width: 150, justifySelf: "center" }}
-                    required
-                    onChange={(e) => setAno(e.target.value)}
-                  />
-                  <Button
-                    variant="contained"
-                    sx={{ width: 150, justifySelf: "left" }}
-                    onClick={handleFetchDataSprint}
-                  >
-                    Pesquisar
-                  </Button>
-                </Grid>
-
-                <Grid container direction="column" spacing={4} marginTop={2}>
+                <Grid container direction="column" spacing={4}>
                   <Grid item sx={{ alignSelf: "center", width: 500 }}>
-                    <InputLabel id="sprint-select-label">Sprint</InputLabel>
                     <Select
                     label="Sprint"
                     variant="outlined"
@@ -128,9 +107,7 @@ function GestaoSprint() {
                     onChange={event => setSprintId(event.value)}
                     />
                   </Grid>
-
                   <Grid item sx={{ alignSelf: "center", width: 500 }}>
-                    <InputLabel id="equipe-select-label">Equipe</InputLabel>
                     <Select
                     label="Equipe"
                     variant="outlined"
